7a8ca9a051c2779b3387205563924b1e1fa5e1f6
Viktor_Zherdev <viktor_zherdev@epam.com>
2021-01-10 22:37:16 +0300

    updated tests

--- src/test/java/utililty/BaseSetup.java
+++ src/test/java/utililty/BaseSetup.java
@@ -5,29 +5,28 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 
 import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
-
 import static utililty.Config.get;
 import static utililty.Driver.runLocal;
 import static utililty.Driver.runRemote;
 import static utililty.Log.infoLog;
 
 public class BaseSetup {
-  final String testRunOption = get("testRunOption");
+    final String testRunOption = get("testRunOption");
 
-  @Parameters({"browser"})
-  @BeforeTest
-  public void setUp(String browser) {
-    if (testRunOption.equals("remote")) {
-      runRemote(browser);
-    } else if (testRunOption.equals("local")) {
-      runLocal();
-    } else {
-      infoLog("Select test run option: remote or local");
+    @Parameters({"browser"})
+    @BeforeTest
+    public void setUp(String browser) {
+        if (testRunOption.equals("remote")) {
+            runRemote(browser);
+        } else if (testRunOption.equals("local")) {
+            runLocal();
+        } else {
+            infoLog("Select test run option: remote or local");
+        }
     }
-  }
 
-  @AfterTest
-  public void tearDown() {
-    closeWebDriver();
-  }
+    @AfterTest
+    public void tearDown() {
+        closeWebDriver();
+    }
 }
